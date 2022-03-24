f401b37199a011e0011c56d2175a71cb312f3f5a
Tkul217 <101782174+Tkul217@users.noreply.github.com>
2022-03-24 10:01:33 +0600

    Create 3.py

---
+++ lab_3/3.py
@@ -0,0 +1,15 @@
+a=int(input("Введите первое число: "))
+b=int(input("Введите второе число: "))
+c=int(input("Введите третье число: "))
+if a>=1 and a<=3:
+    print(a)
+else:
+    print("Число ",a, " не в интервале")
+if b>=1 and b<=3:
+    print(b)
+else:
+    print("Число ",b, " не в интервале")
+if c>=1 and c<=3:
+    print(c)
+else:
+    print("Число ",c, " не в интервале")
